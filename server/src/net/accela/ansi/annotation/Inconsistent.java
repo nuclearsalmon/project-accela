be098e4038fe7d8354fe37abc5925dee3f6f0d04
gustavdersjo <gustav.dersjo@gmail.com>
2020-11-18 13:06:23 +0100

    Improved documentation

--- server/src/net/accela/ansi/annotation/Inconsistent.java
+++ server/src/net/accela/ansi/annotation/Inconsistent.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Interface to mark codes where poor or inconsistent implementations are common.
+ * Interface to mark escape codes where poor or inconsistent implementations are common.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.MODULE, ElementType.PARAMETER, ElementType.TYPE})
--- server/src/net/accela/ansi/annotation/NonStandard.java
+++ server/src/net/accela/ansi/annotation/NonStandard.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Interface to mark non-standard codes
+ * Interface to mark non-standard escape codes
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.MODULE, ElementType.PARAMETER, ElementType.TYPE})
