28aac9281d3cb9c59a4c5f85d03e2da2e52ce82e
gustavdersjo <gustav.dersjo@gmail.com>
2020-11-19 20:45:37 +0100

    Added more of the code to git

--- prismaPlugin/src/net/accela/prisma/Main.java
+++ prismaPlugin/src/net/accela/prisma/Main.java
@@ -3,7 +3,7 @@ package net.accela.prisma;
 import net.accela.server.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    // Necessary for registering listeners later from WindowManager
+    // Necessary for registering listeners later from the window manager
     private static Main instance;
 
     @Override
